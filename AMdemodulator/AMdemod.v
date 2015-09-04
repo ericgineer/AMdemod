5c93766b5e3b3c9affcf6d4dcdcd535cd6ca8914
WestCoastDSP <radioericwilson@gmail.com>
2015-09-03 23:14:20 -0700

    Made an architecture change due to difficulties with the large sample rate changes (simulations were running way too long). Both the I and Q channels now have separate CIC and FIR filter stages to provide a 1 MHz I and Q signal for the AM demodulation stage. A separate filter/decimator stage will be used in this new AM demodulation section.

--- AMdemodulator/AMdemod.v
+++ AMdemodulator/AMdemod.v
@@ -7,9 +7,10 @@ module AMdemod(input wire clk,
 			   input wire rst);
 
    wire signed [15:0] sine, cosine;
-   wire signed [7:0] cic_out;
+   wire signed [7:0] cic_I_out, cic_Q_out;
    reg signed [23:0] I_tmp, Q_tmp;
    reg signed [7:0] I, Q;
+   wire signed [17:0] I_out, Q_out;
    reg signed [15:0] sig_squared;
    
    wire cic_out_clk;
@@ -26,17 +27,38 @@ module AMdemod(input wire clk,
 		 .out_valid(),
 		 .reset_n(~rst));
 		 
-	CIC CIC(.clk(clk),
+	CIC #(.width(42)) CIC_I(.clk(clk),
 			.rst(rst),
-			.decimation_ratio(16'd125), // Downsample to 10 kHz
-	        .d_in(sig_squared >>> 8),
-	        .d_out(cic_out),
+			.decimation_ratio(16'd125), // Downsample to 1 MHz
+	        .d_in(I),
+	        .d_out(cic_I_out),
 			.d_clk(cic_out_clk));
 			
-	SerialFIR FIR(.clk(cic_out_clk),
+	SerialFIR FIR_I(.clk(cic_out_clk),
                   .clk_25x(clk_25x),
                   .rst(rst),
-                  .x(cic_out),
+                  .x(cic_I_out),
+                  .y(I_out),
+                  .clk_out(demod_clk));
+                  
+    CIC #(.width(42)) CIC_Q(.clk(clk),
+			.rst(rst),
+			.decimation_ratio(16'd125), // Downsample to 1 MHz
+	        .d_in(Q),
+	        .d_out(cic_Q_out),
+			.d_clk(cic_out_clk));
+			
+	SerialFIR FIR_Q(.clk(cic_out_clk),
+                  .clk_25x(clk_25x),
+                  .rst(rst),
+                  .x(cic_Q_out),
+                  .y(Q_out),
+                  .clk_out(demod_clk));
+                  
+    SerialFIR FIRdemod(.clk(cic_out_clk),
+                  .clk_25x(clk_25x),
+                  .rst(rst),
+                  .x(sig_squared >>> 8),
                   .y(demod_out),
                   .clk_out(demod_clk));
 			  
@@ -74,7 +96,7 @@ module AMdemod(input wire clk,
 			Q_tmp <= sine   * adc_data;
 			I <= I_tmp >>> 15;
 			Q <= Q_tmp >>> 15;
-			sig_squared <= I * I + Q * Q;
+			sig_squared <= (I_out >>> 7) * (I_out >>> 7) + (Q_out >>> 7) * (Q_out >>> 7);
 		end
 	end
 
--- IP/CIC/CIC.v
+++ IP/CIC/CIC.v
@@ -1,8 +1,6 @@
 `timescale 1ns/1ns
 
-`define width 41
-
-module CIC(
+module CIC #(parameter width = 41)(
 	input wire               clk,
 	input wire               rst,
 	input wire        [15:0] decimation_ratio,
@@ -11,24 +9,24 @@ module CIC(
 	output reg 				 d_clk
 );
 
-reg signed [`width-1:0] d_tmp, d_d_tmp;
+reg signed [width-1:0] d_tmp, d_d_tmp;
 
 
 // Integrator stage registers
 
-reg signed [`width-1:0] d1;
-reg signed [`width-1:0] d2;
-reg signed [`width-1:0] d3;
-reg signed [`width-1:0] d4;
-reg signed [`width-1:0] d5;
+reg signed [width-1:0] d1;
+reg signed [width-1:0] d2;
+reg signed [width-1:0] d3;
+reg signed [width-1:0] d4;
+reg signed [width-1:0] d5;
 
 // Comb stage registers
 
-reg signed [`width-1:0] d6, d_d6;
-reg signed [`width-1:0] d7, d_d7;
-reg signed [`width-1:0] d8, d_d8;
-reg signed [`width-1:0] d9, d_d9;
-reg signed [`width-1:0] d10;
+reg signed [width-1:0] d6, d_d6;
+reg signed [width-1:0] d7, d_d7;
+reg signed [width-1:0] d8, d_d8;
+reg signed [width-1:0] d9, d_d9;
+reg signed [width-1:0] d10;
 
 reg [15:0] count;
 
@@ -66,24 +64,20 @@ reg d_clk_tmp;
 			begin
 				count <= 16'b0;
 				d_tmp <= d5;
-				d_clk_tmp <= 1'b1;
-				v_comb <= 1'b1;
+				d_clk <= 1'b1;
 			end else if (count == decimation_ratio >> 1)
 			begin
-				d_clk_tmp <= 1'b0;
+				d_clk <= 1'b0;
 				count <= count + 16'd1;
-				v_comb <= 1'b0;
 			end else
 			begin
 				count <= count + 16'd1;
-				v_comb <= 1'b0;
 			end
 		end
 	end
 	
-	always @(posedge clk)  // Comb section running at output rate
+	always @(posedge d_clk)  // Comb section running at output rate
 	begin
-		d_clk <= d_clk_tmp;
 		if (rst)
 		begin
 			d6 <= 0;
@@ -98,27 +92,24 @@ reg d_clk_tmp;
 			d_out <= 8'b0;
 		end else
 		begin
-			if (v_comb)
-			begin
-				// Comb section
-				d_d_tmp <= d_tmp;
-				
-				d6 <= d_tmp - d_d_tmp;
-				d_d6 <= d6;
+			// Comb section
+			d_d_tmp <= d_tmp;
+			
+			d6 <= d_tmp - d_d_tmp;
+			d_d6 <= d6;
 
-				d7 <= d6 - d_d6;
-				d_d7 <= d7;
+			d7 <= d6 - d_d6;
+			d_d7 <= d7;
 
-				d8 <= d7 - d_d7;
-				d_d8 <= d8;
+			d8 <= d7 - d_d7;
+			d_d8 <= d8;
 
-				d9 <= d8 - d_d8;
-				d_d9 <= d9;
+			d9 <= d8 - d_d8;
+			d_d9 <= d9;
 
-				d10 <= d9 - d_d9;
-				
-				d_out <= d10 >>> (`width - 8);
-			end
+			d10 <= d9 - d_d9;
+			
+			d_out <= d10 >>> (width - 8);
 		end
 	end								
 endmodule
